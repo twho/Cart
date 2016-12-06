1cdf239e801d7e52f4f954f7c72701e7c3c3f862
Michael Ho <tsungwei50521@gmail.com>
2016-12-06 00:25:48 -0500

    update to safari browser

---
+++ workspace_CARt/CARt/CARt/WebViewController.swift
@@ -0,0 +1,44 @@
+//
+//  WebViewController.swift
+//  CARt
+//
+//  Created by Michael Ho on 12/5/16.
+//  Copyright © 2016 cartrides.org. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var edWebUrl: UITextField!
+    @IBOutlet weak var btnSearch: UIButton!
+    //WebToHomeIdentifier
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.edWebUrl.delegate = self
+        self.hideKeyboardWhenTappedAround()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if (textField.returnKeyType==UIReturnKeyType.go)
+        {
+            self.performSegue(withIdentifier: "WebToHomeIdentifier", sender: self)
+        }
+        return true
+    }
+    
+    @IBAction func startTyping(_ sender: UITextField) {
+        self.edWebUrl.placeholder = ""
+    }
+    
+    @IBAction func edFocused(_ sender: UITextField) {
+        btnSearch.isHidden = true
+        self.edWebUrl.placeholder = "Search or enter website name"
+    }
+    
+}
